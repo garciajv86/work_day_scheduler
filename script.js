94d0e9da450d4d587a0220bb82522f2921121d39
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-04-16 13:14:48 -0500

    created a save, load functions and event listener for the save button

--- script.js
+++ script.js
@@ -1,18 +1,65 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-
-
 $(function () {
 
   var currentDay = $('#currentDay');
+  var saveBtn = $('.saveBtn');
+  
+  // Save event function to local storage
+  function saveEvent() {
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+      var desc = $('#9 .description').val();
+      localStorage.setItem('desc9', desc);
+
+      var desc = $('#10 .description').val();
+      localStorage.setItem('desc10', desc);
+
+      var desc = $('#11 .description').val();
+      localStorage.setItem('desc11', desc);
+
+      var desc = $('#12 .description').val();
+      localStorage.setItem('desc12', desc);
+
+      var desc = $('#13 .description').val();
+      localStorage.setItem('desc13', desc);
+
+      var desc = $('#14 .description').val();
+      localStorage.setItem('desc14', desc);
+
+      var desc = $('#15 .description').val();
+      localStorage.setItem('desc15', desc);
+
+      var desc = $('#16 .description').val();
+      localStorage.setItem('desc16', desc);
+
+      var desc = $('#17 .description').val();
+      localStorage.setItem('desc17', desc);
+
+  };
+
+  // Load event function from local storage
+  function loadEvent() {
+
+    $('#9 .description').val(localStorage.getItem('desc9'));
+    $('#10 .description').val(localStorage.getItem('desc10'));
+    $('#11 .description').val(localStorage.getItem('desc11'));
+    $('#12 .description').val(localStorage.getItem('desc12'));
+    $('#13 .description').val(localStorage.getItem('desc13'));
+    $('#14 .description').val(localStorage.getItem('desc14'));
+    $('#15 .description').val(localStorage.getItem('desc15'));
+    $('#16 .description').val(localStorage.getItem('desc16'));
+    $('#17 .description').val(localStorage.getItem('desc17'));
+
+  };
+
+  
+  // Event listener on the save button on click to run the saveEvent() function
+  saveBtn.on('click', function(event) {
+    event.preventDefault();
+    saveEvent();
+  });
+
   
   // Got this idea from stack overflow
   function timeBlock () {
@@ -42,14 +89,8 @@ $(function () {
   })};
 
   timeBlock();
-  
+  loadEvent();
 
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   //Add code to display the current date in the header of the page.
   currentDay.text(dayjs().format('dddd, MMMM DD'));
 });
